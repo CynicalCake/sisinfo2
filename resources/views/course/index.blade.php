df72ac4517a593c55a2dc5430eca0d8a44277bb4
Carlos Vargas <esteban.vg00@gmail.com>
2024-04-11 16:28:22 -0400

    fix: cambios de nombre en algunas pestañas

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Plataforma de Estudios</title>
+    <title>Clases disponibles</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -36,11 +36,11 @@
     <div class="row">
         @foreach ($courses as $course)
             <div class="col-md-4">
-                <div class="card">
+                <div class="card mt-3">
                     <div class="card-body">
                         <h5 class="card-title">{{ $course->name }}</h5>
                         <p class="card-text">{{ $course->description }}</p>
-                        <a href="{{ route('courses.show', $course->code) }}" class="btn btn-primary">Ir al Curso</a>
+                        <a href="{{ route('courses.show', $course->code) }}" class="btn btn-primary">Ir al curso</a>
                     </div>
                 </div>
             </div>
